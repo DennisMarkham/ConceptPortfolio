e9d1000d4f291db74d6d72a23e58dc05cce7b7bc
DennisMarkham <workdennis@yahoo.com>
2019-03-20 16:23:36 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -20,6 +20,7 @@ function holiday()
   $("link[href='style.css']").attr('href','easter.css');
   $(".holiday").attr('onclick', 'unholiday();')
    localStorage.setItem("holiday", "holiday");
+   bunnyHop();
 }
 
 function unholiday()
@@ -29,6 +30,57 @@ function unholiday()
   $(".holiday").attr('onclick', 'holiday();')
   localStorage.setItem("holiday", "unholiday");
 }
+
+// function bunnyHop()
+// {
+//   alert("bunny is hopping")
+//   var elem = document.getElementById("bunny");
+//   //I am isolating the two components of the animation.  This is how the bunny moves left:
+//   //****
+   
+//   // var horPos = 0;
+//   // var windowWidth = 0;
+//   // //will use this later ^
+//   // var id = setInterval(moveLeft, 10);
+//   // function moveLeft() {
+//   //   if (horPos == 1000) {
+//   //     clearInterval(id);
+//   //   } else {
+//   //     horPos++; 
+//   //     elem.style.left = horPos + 'px'; 
+//   //   }
+//   // }
+// //****
+// var upPos = 0;
+
+// var id2 = setInterval(moveUp, 10);
+//   function moveUp() {
+//     if (upPos == 100) {
+//       // clearInterval(id2);
+//       moveDown();
+//     } else {
+//       upPos++; 
+//       elem.style.bottom = upPos + 'px'; 
+//     }
+//   }
+
+
+//   var downPos = 0;
+
+
+//   function moveDown() {
+//     if (downPos == 100) {
+//       // clearInterval(id3);
+//       downPos = 0;
+//       moveUp();
+//     } else {
+//       downPos--; 
+//       elem.style.bottom = downPos + 'px'; 
+//     }
+//   }
+
+
+// }
 //*********
 //*****HOME STUFF******
 
@@ -109,6 +161,9 @@ function nextImage() {
 }
 
 
+console.log("age test:" + moment("20111031", "YYYYMMDD").fromNow());
+
+$("#age").text("lklj");
 
 var hours = parseFloat(moment().startOf('day').fromNow());
 // var hours = moment('HH');
@@ -130,113 +185,113 @@ else
 //*****************
 //****PORTFOLIO STUFF****
 
-//on click of .thumbnail a local storage variable with the name of this.id is set to true
-//first need to set the variables in local storage and make them false...right?
+// //on click of .thumbnail a local storage variable with the name of this.id is set to true
+// //first need to set the variables in local storage and make them false...right?
 
 
-$(".thumbnail").click(function()
-{
+// $(".thumbnail").click(function()
+// {
 
-  console.log("function firing: setting " + this.id + " to true");
+//   console.log("function firing: setting " + this.id + " to true");
 
-  localStorage.setItem(this.id, "visited");
-//set visited to true
-})
-//QUESTIONS:
-//-Train sound and magic sound
-var trainSound = new Audio("train.wav");
-  trainSound.volume = 0.1;
-  var crystalSound = new Audio("crystal.wav");
-  crystalSound.volume = 0.25;
-   var shutterSound = new Audio("shutter.wav");
-   shutterSound.volume = 0.2;
-  var thunderSound = new Audio("thunder.wav");
-  thunderSound.volume = 0.15;
-  var boomSound = new Audio("boom.mp3");
-  boomSound.volume = 0.15;
-  var typingSound = new Audio("typing.wav");
-  typingSound.volume = 0.15;
-  var wipingSound = new Audio("wiping.wav");
-  wipingSound.volume = 0.5;
-  var beepSound = new Audio("beep.wav");
-  beepSound.volume = 0.2;
+//   localStorage.setItem(this.id, "visited");
+// //set visited to true
+// })
+// //QUESTIONS:
+// //-Train sound and magic sound
+// var trainSound = new Audio("train.wav");
+//   trainSound.volume = 0.1;
+//   var crystalSound = new Audio("crystal.wav");
+//   crystalSound.volume = 0.25;
+//    var shutterSound = new Audio("shutter.wav");
+//    shutterSound.volume = 0.2;
+//   var thunderSound = new Audio("thunder.wav");
+//   thunderSound.volume = 0.15;
+//   var boomSound = new Audio("boom.mp3");
+//   boomSound.volume = 0.15;
+//   var typingSound = new Audio("typing.wav");
+//   typingSound.volume = 0.15;
+//   var wipingSound = new Audio("wiping.wav");
+//   wipingSound.volume = 0.5;
+//   var beepSound = new Audio("beep.wav");
+//   beepSound.volume = 0.2;
   
-  function showStuff() {
-    $("#content").fadeIn("slow");
+//   function showStuff() {
+//     $("#content").fadeIn("slow");
 
-    //visit tracker:
+//     //visit tracker:
 
-    var visitedArray = [];
-    var keysArray =[];
+//     var visitedArray = [];
+//     var keysArray =[];
 
   
 
-//okay, okay, the visitedArray is only passed "visited" or "null"
-//so I think what we need is parrelel array of keys.  
-//alright, let's start simple, we can dry this up later.  AND I THINK
-//TO DO THAT WE WILL HAVE TO USE THE KEYS ARRAY TO BUILD THE VISITED ARRAY!
-//YES!
+// //okay, okay, the visitedArray is only passed "visited" or "null"
+// //so I think what we need is parrelel array of keys.  
+// //alright, let's start simple, we can dry this up later.  AND I THINK
+// //TO DO THAT WE WILL HAVE TO USE THE KEYS ARRAY TO BUILD THE VISITED ARRAY!
+// //YES!
 
-keysArray.push("crystalCollector");
-keysArray.push("trainTimer");
-keysArray.push("bombDefuser");
-keysArray.push("weatherMonitor");
-keysArray.push("reactGallery");
-keysArray.push("roulette");
-keysArray.push("whiteboard");
-keysArray.push("chatroom");
-keysArray.push("profanity");
+// keysArray.push("crystalCollector");
+// keysArray.push("trainTimer");
+// keysArray.push("bombDefuser");
+// keysArray.push("weatherMonitor");
+// keysArray.push("reactGallery");
+// keysArray.push("roulette");
+// keysArray.push("whiteboard");
+// keysArray.push("chatroom");
+// keysArray.push("profanity");
 
-for (i = 0; i < keysArray.length; i++)
-{
-visitedArray.push(localStorage.getItem(keysArray[i]));
-}
+// for (i = 0; i < keysArray.length; i++)
+// {
+// visitedArray.push(localStorage.getItem(keysArray[i]));
+// }
 
-//create a for loop to loop through array, maybe it logs caption text into the 
-//console?
-for (i = 0; i < visitedArray.length; i++) { 
-   if (visitedArray[i] == "visited")
-     {
-       console.log(keysArray[i] + " visited");
-       $("#" + keysArray[i]).css("opacity", "0.5");
-    }
+// //create a for loop to loop through array, maybe it logs caption text into the 
+// //console?
+// for (i = 0; i < visitedArray.length; i++) { 
+//    if (visitedArray[i] == "visited")
+//      {
+//        console.log(keysArray[i] + " visited");
+//        $("#" + keysArray[i]).css("opacity", "0.5");
+//     }
 
-}
- //okay, the above just logs visited, not the same of the variable in local storage   
- //EFF YEAH!  YOU DID IT!!
-}
+// }
+//  //okay, the above just logs visited, not the same of the variable in local storage   
+//  //EFF YEAH!  YOU DID IT!!
+// }
 
 
- function train(){
-  trainSound.play();
-  console.log("Playing train");
- }
- function crystal(){
-  crystalSound.play();
-  console.log("Playing crystal");
- }
- function shutter(){
- shutterSound.play();
- console.log("Playing shutter");
- }
- function thunder(){
-  thunderSound.play();
-  console.log("Playing thunder");
- }
- function boom(){
-  boomSound.play();
-  console.log("Playing boom");
- }
-function typing(){
-  typingSound.play();
-  console.log("playing typing");
-}
-function wiping(){
-  wipingSound.play();
-  console.log("playing wiping");
-}
-function beep(){
-  beepSound.play();
-  console.log("playing beep");
-}
-//*********************
+//  function train(){
+//   trainSound.play();
+//   console.log("Playing train");
+//  }
+//  function crystal(){
+//   crystalSound.play();
+//   console.log("Playing crystal");
+//  }
+//  function shutter(){
+//  shutterSound.play();
+//  console.log("Playing shutter");
+//  }
+//  function thunder(){
+//   thunderSound.play();
+//   console.log("Playing thunder");
+//  }
+//  function boom(){
+//   boomSound.play();
+//   console.log("Playing boom");
+//  }
+// function typing(){
+//   typingSound.play();
+//   console.log("playing typing");
+// }
+// function wiping(){
+//   wipingSound.play();
+//   console.log("playing wiping");
+// }
+// function beep(){
+//   beepSound.play();
+//   console.log("playing beep");
+// }
+// //*********************
