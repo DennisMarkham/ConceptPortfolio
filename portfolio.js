dfc99148893f4aceabc605865229144b7f362de3
DennisMarkham <workdennis@yahoo.com>
2019-03-21 14:39:11 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -31,33 +31,75 @@ function unholiday()
   localStorage.setItem("holiday", "unholiday");
 }
 
-// function bunnyHop()
-// {
-//   alert("bunny is hopping")
-//   var elem = document.getElementById("bunny");
+function bunnyHop()
+{
+  var elem = document.getElementById("bunny");
 //   //I am isolating the two components of the animation.  This is how the bunny moves left:
 //   //****
    
-//   // var horPos = 0;
-//   // var windowWidth = 0;
-//   // //will use this later ^
-//   // var id = setInterval(moveLeft, 10);
-//   // function moveLeft() {
-//   //   if (horPos == 1000) {
-//   //     clearInterval(id);
-//   //   } else {
-//   //     horPos++; 
-//   //     elem.style.left = horPos + 'px'; 
-//   //   }
-//   // }
+  // var horPos = 0;
+  // var windowWidth = 0;
+  // //will use this later ^
+  // var id = setInterval(moveLeft, 10);
+  // function moveLeft() {
+  //   if (horPos == 1000) {
+  //     clearInterval(id);
+  //   } else {
+  //     horPos++; 
+  //     elem.style.left = horPos + 'px'; 
+  //   }
+  // }
 // //****
+
+//START FROM SCRATCH:
+//BUNNY MUST MOVE UP, THEN MOVE DOWN
+function verticalMotion()
+{
+var id2 = setInterval(moveUp, 3);
+var upPos = 0;
+function moveUp()
+{
+  if (upPos == 100)
+  {
+    clearInterval(id2);
+    var id3 = setInterval(moveDown, 2);
+  }
+  else
+  {
+    upPos++;
+    elem.style.bottom = upPos + 'px';
+  }
+}
+
+var downPos = 100;
+
+function moveDown()
+{
+  if (downPos == 0)
+  {
+    clearInterval(id3)
+  }
+  else
+  {
+  downPos--;
+  elem.style.bottom = downPos + 'px';
+}
+}
+}
+
+var id4 = setInterval(verticalMotion, 5000);
+// verticalMotion();
+
 // var upPos = 0;
+
+// var goingUp = true;
 
 // var id2 = setInterval(moveUp, 10);
 //   function moveUp() {
-//     if (upPos == 100) {
+//     if (upPos == 100 && goingUp == true) {
 //       // clearInterval(id2);
-//       moveDown();
+//       goingUp = false;
+//       moveDown();      
 //     } else {
 //       upPos++; 
 //       elem.style.bottom = upPos + 'px'; 
@@ -65,22 +107,21 @@ function unholiday()
 //   }
 
 
-//   var downPos = 0;
+  
 
 
 //   function moveDown() {
-//     if (downPos == 100) {
+//     if (upPos == 0) {
 //       // clearInterval(id3);
-//       downPos = 0;
 //       moveUp();
 //     } else {
-//       downPos--; 
-//       elem.style.bottom = downPos + 'px'; 
+//       upPos--; 
+//       elem.style.bottom = upPos + 'px'; 
 //     }
-//   }
+  // }
 
 
-// }
+}
 //*********
 //*****HOME STUFF******
 
