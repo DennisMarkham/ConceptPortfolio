f93e8e0c2a39db5d393f79c427ec623e90be53c6
DennisMarkham <workdennis@yahoo.com>
2022-09-05 18:33:44 -0500

    Redesigned code for assigning style-sheet
    This new code should make it much easier to add skins my page.

--- portfolio.js
+++ portfolio.js
@@ -6,27 +6,29 @@ function showStuff() {
 //this does not have any effect until I re-enable the "display none" on my css.  But
 //this is faulty for some reason
 
-function holidayCheck()
-{
-if (localStorage.getItem("holiday") == "easter")
-{
-  $("link[href='default.css']").attr('href','easter.css');
-  $('select option[value="easter"]').attr("selected",true);
-}
-else
-{
-  $("link[href='easter.css']").attr('href','default.css');
-    $('select option[value="default"]').attr("selected",true);
-}
-}
-
-//this streamlined version of holidayCheck breaks the program for some reason
 // function holidayCheck()
 // {
-// var skin = localStorage.getItem("holiday");
-
-// $("link[rel='stylesheet']").attr('href', skin + '.css');
+// if (localStorage.getItem("holiday") == "easter")
+// {
+//   $("link[href='default.css']").attr('href','easter.css');
+//   $('select option[value="easter"]').attr("selected",true);
 // }
+// else
+// {
+//   $("link[href='easter.css']").attr('href','default.css');
+//     $('select option[value="default"]').attr("selected",true);
+// }
+// }
+
+// this streamlined version of holidayCheck breaks the program for some reason
+//
+function holidayCheck()
+{
+var skin = localStorage.getItem("holiday");
+
+$("link[type='text/css']").attr('href', skin + '.css');
+ $('select option[value="' + skin + '"]').attr("selected",true);
+}
 
 
 function setHoliday()
