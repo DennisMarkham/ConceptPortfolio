2fc329d31b74d9845e5d3f3f8285130ab059d51f
unknown <workdennis@yahoo.com>
2023-06-10 16:54:14 -0500

    bouncing bunny finally works

--- portfolio.js
+++ portfolio.js
@@ -22,44 +22,44 @@ function setSkin() {
     skinCheck();
 }
 
-function bunnyHop()
-{
-  var elem = document.getElementById("bunny");
+// function bunnyHop()
+// {
+//   var elem = document.getElementById("bunny");
 
-function verticalMotion()
-{
-var id2 = setInterval(moveUp, 3);
-var upPos = 0;
-function moveUp()
-{
-  if (upPos == 100)
-  {
-    clearInterval(id2);
-    var id3 = setInterval(moveDown, 2);
-  }
-  else
-  {
-    upPos++;
-    elem.style.bottom = upPos + 'px';
-  }
-}
+// function verticalMotion()
+// {
+// var id2 = setInterval(moveUp, 3);
+// var upPos = 0;
+// function moveUp()
+// {
+//   if (upPos == 100)
+//   {
+//     clearInterval(id2);
+//     var id3 = setInterval(moveDown, 2);
+//   }
+//   else
+//   {
+//     upPos++;
+//     elem.style.bottom = upPos + 'px';
+//   }
+// }
 
-var downPos = 100;
+// var downPos = 100;
 
-function moveDown()
-{
-  if (downPos != 0)
-  {
-    downPos--;
-  elem.style.bottom = downPos + 'px';
-  }
+// function moveDown()
+// {
+//   if (downPos != 0)
+//   {
+//     downPos--;
+//   elem.style.bottom = downPos + 'px';
+//   }
   
-}
-}
+// }
+// }
 
-var id4 = setInterval(verticalMotion, 5000);
+// var id4 = setInterval(verticalMotion, 5000);
 
 
-}
+// }
 
   
